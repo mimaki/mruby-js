0698882847816f22ee065c824bbc6381bf8f875c
Hiroshi Mimaki <hiroshi_mimaki@fukuoka-csk.co.jp>
2013-04-13 14:28:39 +0900

    remove duplicate function

--- src/mruby_js.c
+++ src/mruby_js.c
@@ -460,8 +460,9 @@ mrb_js_funcall_argv(mrb_state *mrb, const char *func, int argc, mrb_value *argv)
 //printf("JsObject: ");
 //mrb_p(mrb, jso);
 
-  js_call(mrb, mruby_js_get_object_handle_value(mrb, jso),
-          func, argv, argc, &r, 0);
+  // js_call(mrb, mruby_js_get_object_handle_value(mrb, jso),
+  //         func, argv, argc, &r, 0);
+  r = mrb_funcall_argv(mrb, jso, mrb_intern(mrb, func), argc, argv);
 
 //printf("mrb_js_funcall_argv() end.\n");
 
@@ -506,27 +507,6 @@ printf("mrb_js_funcall() start.\n");
     r = mrb_js_funcall_argv(mrb, func, argc, argv);
   }
 printf("mrb_js_funcall() end.\n");
-  return r;
-}
-
-mrb_value
-mrb_js_funcall_argv(mrb_state *mrb, const char *func, int argc, mrb_value *argv)
-{
-  mrb_value mjs, jso, r;
-
-//printf("mrb_js_funcall_argv() start.\n");
-  mjs = mrb_const_get(mrb, mrb_obj_value(mrb->object_class), mrb_intern(mrb, "MrubyJs"));
-
-  jso = mrb_js_get_root_object(mrb, mjs);
-//printf("JsObject: ");
-//mrb_p(mrb, jso);
-
-  // js_call(mrb, mruby_js_get_object_handle_value(mrb, jso),
-  //         func, argv, argc, &r, 0);
-  r = mrb_funcall_argv(mrb, jso, mrb_intern(mrb, func), argc, argv);
-
-//printf("mrb_js_funcall_argv() end.\n");
-
   return r;
 }
 
